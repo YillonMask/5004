5f85c38440b040ac7d8ed597f224a61395e8a9b0
Xinrui Yi <yixinrui24@gmail.com>
2024-01-10 15:37:13 -0800

    lab0

--- lab0/untitled/src/Main.java
+++ lab0/untitled/src/Main.java
@@ -1,10 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+    CS5004_5005 Fall 2023 SV
+    Lab00
+    Xinrui Yi
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello world!");
-
     }
 }
