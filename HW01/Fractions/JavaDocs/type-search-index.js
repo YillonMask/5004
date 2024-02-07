8ef072a332e95ce52930ffb1a6e8985abbd871d5
Xinrui Yi <yixinrui24@gmail.com>
2024-02-07 08:50:54 -0800

    javadoc

---
+++ HW01/Fractions/JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Fraction"},{"p":"<Unnamed>","l":"FractionImpl"},{"p":"<Unnamed>","l":"FractionImplTest"}];updateSearchResults();
