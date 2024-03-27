b51e0b536ed4546dfe80f4e0aa05260dae21f4f0
Xinrui Yi <yixinrui24@gmail.com>
2024-03-27 14:22:56 -0700

    updates

---
+++ lab09/lab09/src/Coordinate.java
@@ -0,0 +1,19 @@
+public class Coordinate {
+  public Coordinate(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  int x;
+  int y;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Coordinate) {
+      Coordinate other = (Coordinate) obj;
+      return other.x == this.x && other.y == this.y;
+    }
+
+    return false;
+  }
+}
